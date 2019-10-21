a3b77973801381faccb51f7be8028bffdddaece2
BillAouissat <baouissat@bigapps.fr>
2019-10-21 11:29:01 +0200

    premier push

--- src/main/scala/com/test/spark/wiki/extracts/HelloWorld.scala
+++ src/main/scala/com/test/spark/wiki/extracts/HelloWorld.scala
@@ -1,5 +1,5 @@
 package com.test.spark.wiki.extracts
-
+import org.apache.spark.{SparkConf, SparkContext}
 
 import org.apache.spark.sql.{Dataset, SaveMode, SparkSession}
 
@@ -7,12 +7,24 @@ object  HelloWorld {
 
   def main(args: Array[String]): Unit = {
 
-    implicit  val  spark: SparkSession=SparkSession
-      .builder()
-      .master("local[*]")
-      .getOrCreate()
 
-    println("Hello World")
+
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("Word Count")
+    val sc = new SparkContext(conf)
+
+    // Load the text into a Spark RDD, which is a distributed representation of each line of text
+    val textFile = sc.textFile("C:/spark/spark-2.4.4-bin-hadoop2.7/README.md")
+
+    //word count
+    val counts = textFile.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+
+    counts.foreach(println)
+    System.out.println("Total words: " + counts.count());
+    counts.saveAsTextFile("/tmp/shakespeareWordCount")
   }
 
 
